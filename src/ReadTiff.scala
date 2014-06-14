d82400e4ab395f8a1d5ccad854aa58fc8d0f72ec
Kentaro Kuwata <kuwaken0925@gmail.com>
2014-06-14 13:00:07 +0900

    2014.06.14 Committed Next time, trying to develop by using C++

---
+++ src/ReadTiff.scala
@@ -0,0 +1,13 @@
+import net.imglib2.img.Img
+import net.imglib2.img.array.ArrayImgFactory
+import net.imglib2
+import net.imglib2.`type`.numeric.integer.UnsignedByteType
+
+import io.scif.config.SCIFIOConfig
+import io.scif.config.SCIFIOConfig.ImgMode
+import io.scif.img.ImgIOException
+import io.scif.img.ImgOpener
+
+object ReadTiff {
+
+}
--- src/wavelet_test.scala
+++ src/wavelet_test.scala
@@ -1,3 +1,5 @@
+
+
 object wavelet_test {
 
 }
